a65f3af2a2b66c57a8862e8b55652c6bf8aec8bc
ricky <ricky@DESKTOP-1S8IBRV.cable.rcn.com>
2018-12-06 00:23:32 -0500

    completed objectivetype and base

--- src/com/sms/ObjectiveType.java
+++ src/com/sms/ObjectiveType.java
@@ -1,5 +1,28 @@
 package com.sms;
 
-public class ObjectiveType {
+public class ObjectiveType extends Base{
 
+// common attributes 
+String name; 
+int creation_date; 
+int updated_date; 	
+String active; 
+
+//uncommon 
+String creator_name;
+String attribute_list;
+
+
+public ObjectiveType(String name, int creation_date, int updated_date, String active, String creator_name, String attribute_list) {
+	super(name,creation_date,updated_date, active);
+	this.creator_name=creator_name;
+	this.attribute_list=attribute_list;
+	
+}
+public void printObjectiveType() {
+
+	System.out.println("Name"+ "/t"+ "Creation_date"+ "/t"+ "Updated_date"+ "/t" + "Active" + "/t" + "creator_name" + "/t" + "attribute_list");
+	System.out.println(name + "/t" + creation_date + "/t" + updated_date + "/t" + active + "/t" + creator_name + "/t" + attribute_list);
+	
+}
 }
