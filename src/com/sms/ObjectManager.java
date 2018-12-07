581e43f32718d306a697477c66015e4acfd50838
ricky <ricky@DESKTOP-1S8IBRV.cable.rcn.com>
2018-12-07 18:01:04 -0500

    Add method added

--- src/com/sms/ObjectManager.java
+++ src/com/sms/ObjectManager.java
@@ -1,33 +1,36 @@
 package com.sms;
-import java.util.*;
 import java.util.ArrayList;
-
-
+import java.util.*;
 public class ObjectManager 
 {
 	public static int size;
 	public static ArrayList<ObjectiveType> ObjTypeList = new ArrayList<ObjectiveType>();
-	 
+	public static Scanner input = new Scanner(System.in);
 	
 
 	public static void operations() 
 	{
 		int userinput;
-		Scanner input = new Scanner(System.in);
+		
+		System.out.println("");
+		System.out.println("Select your operation: ");
 		System.out.println("Press 1: Add");
 		System.out.println("Press 2: Read");
 		System.out.println("Press 3: Update");
 		System.out.println("Press 4: Delete");
+		System.out.println("Press 5: Exit");
 		System.out.println("******************************************************************");
-		System.out.println("");
-		
 		userinput = input.nextInt();
+		
 		switch(userinput) 
 		{
 			case 1:
 				add();
 				break;
-		
+			case 5:
+				System.out.println("Program ended, bye!!!");
+				input.close();
+				break;
 		}
 		
 		
@@ -36,42 +39,45 @@ public class ObjectManager
 
 	public static void add() 
 	{
-		Scanner input = new Scanner(System.in);
-		System.out.println("Select your operation: ");
-		int operation1 = input.nextInt();
-
-		switch (operation1) 
+		System.out.println("How many objects would you like to create: ");
+		int index = input.nextInt();
+	
+		for (int j = 0; j < index; j++)
 		{
-		case 1:
-			operation1 = 1;
-
+			System.out.println("Enter data for Objective Type at index: " + (j+1));
 			
+			System.out.println("Enter String name: ");
+			String name = input.next();
+
+			System.out.println("Enter creation date: ");
+			int createDate = input.nextInt();
+
+			System.out.println("Enter the update date: ");
+			int updateDate = input.nextInt();
+
+			System.out.println("Enter active: ");
+			String active = input.next();
+
+			System.out.println("Enter Creator Name: ");
+			String creatorName = input.next();
+
+			System.out.println("Enter Attribute list: ");
+			String attList = input.next();
 			
-			System.out.println("How many objects would you like to create: ");
-			int ques1 = input.nextInt();
-
-			System.out.println("Enter String name?");
-			String ques2 = input.next();
-
-			System.out.println("Enter creation date?");
-			int ques3 = input.nextInt();
-
-			System.out.println("Enter the update date?");
-			int ques4 = input.nextInt();
-
-			System.out.println("Enter active?");
-			String ques5 = input.next();
-
-			System.out.println("Enter Creator Name?");
-			String ques6 = input.next();
-
-			System.out.println("Enter Attribute list?");
-			String ques7 = input.next();
-
-			for (int i = 0; i < ObjTypeList.size(); i++) {
-			ObjTypeList.get(i).printBase();
-			}
-
+			ObjectiveType obj = new ObjectiveType(name, createDate, updateDate, active, creatorName, attList);
+			ObjTypeList.add(j, obj);
 		}
+		
+		if(index>0)
+		{
+			System.out.println("Name" + "\t" + "Creation_date" + "\t" + "Updated_date" + "\t" + "Active" + "\t"
+					+ "creator_name" + "\t" + "attribute_list");
+			for (int i = 0; i < index; i++) 
+			{
+				ObjTypeList.get(i).printObjectiveType();
+			}
+		}
+		operations();
 	}
 }
+
--- src/com/sms/ObjectiveType.java
+++ src/com/sms/ObjectiveType.java
@@ -6,12 +6,6 @@ public class ObjectiveType extends Base {
 
 	public static ArrayList<ObjectiveType> ObjTypeList = new ArrayList<ObjectiveType>();
 
-// common attributes 
-	 String name;
-	 int creation_date;
-	int updated_date;
-	String active;
-
 //uncommon 
 	String creator_name;
 	String attribute_list;
@@ -27,8 +21,7 @@ public class ObjectiveType extends Base {
 //print method
 	public void printObjectiveType() {
 		
-		System.out.println("Name" + "\t" + "Creation_date" + "\t" + "Updated_date" + "\t" + "Active" + "\t"
-				+ "creator_name" + "\t" + "attribute_list");
+		
 		System.out.println(name + "\t" + "\t" + creation_date + "\t" + "\t" + updated_date + "\t" + active 
 				+ "\t" + creator_name + "\t" + attribute_list);
 
