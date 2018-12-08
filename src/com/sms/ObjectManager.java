326da98caed9891ce93fddab642f1c146c9de468
ricky <ricky@DESKTOP-1S8IBRV.cable.rcn.com>
2018-12-08 16:38:49 -0500

    Read method updated Update method added

--- src/com/sms/ObjectManager.java
+++ src/com/sms/ObjectManager.java
@@ -32,6 +32,9 @@ public class ObjectManager
 			case 3:
 				update();
 				break;
+			case 4:
+				delete();
+				break;
 			case 5:
 				System.out.println("Program ended, bye!!!");
 				input.close();
@@ -82,44 +85,101 @@ public class ObjectManager
 		operations();
 	}
 
-	public static void read() {
-		System.out.println("what index would you like to print out: ");
-		int readindex = input.nextInt();
-
-		for (int g = 0; g < readindex; g++) {
-			System.out.println("Name" + "\t" + "Creation_date" + "\t" + "Updated_date" + "\t" + "Active" + "\t"
-					+ "Creator_name" + "\t" + "Attribute_list");
-			ObjTypeList.get(g).printObjectiveType();
-
+	public static void read() 
+	{
+		if(ObjTypeList.size()>0)
+		{
+			System.out.println("Number of rows in list: " + ObjTypeList.size());
+			System.out.println("Select your read operation option: ");
+			System.out.println("Press 1: Read All");
+			System.out.println("Press 2: Read specific index");
+			System.out.println("******************************************************************");
+			int readOption = input.nextInt();
+			switch(readOption)
+			{
+			case 1:
+				System.out.println("Name" + "\t" + "Creation_date" + "\t" + "Updated_date" + "\t" + "Active" + "\t"
+						+ "Creator_name" + "\t" + "Attribute_list");
+				for (int g = 0; g < ObjTypeList.size(); g++) 
+				{
+					ObjTypeList.get(g).printObjectiveType();
+				}
+				break;
+			case 2:
+				System.out.println("Enter index to read: ");
+				int readIndex = input.nextInt();
+				if(readIndex <= 0 || readIndex > ObjTypeList.size())
+					System.out.println("Invalid Index");
+				else
+				{
+					System.out.println("Name" + "\t" + "Creation_date" + "\t" + "Updated_date" + "\t" + "Active" + "\t"
+								+ "Creator_name" + "\t" + "Attribute_list");
+					ObjTypeList.get(readIndex-1).printObjectiveType();
+				}
+				break;
+			}
 		}
+		else
+			System.out.println("There are no rows in the list");
+
 		operations();
 	}
 	
 	public static void update() 
 	{
-	
-		System.out.println("What index would you like to update: ");
-		int readindex = input.nextInt();
 		System.out.println("Name" + "\t" + "Creation_date" + "\t" + "Updated_date" + "\t" + "Active" + "\t"
 				+ "Creator_name" + "\t" + "Attribute_list");
-			
-		switch (readindex) {
-		case 0:			
-			ObjTypeList.get(0).printObjectiveType();
-			System.out.println("What element would you like to change: ");
-			int element = input.nextInt();			
-		break;
-		
-		
-		case 1:			
-			ObjTypeList.get(1).printObjectiveType();	
-			System.out.println("What element would you like to change: ");
-		 	int element1 = input.nextInt();		 	
-		 
-	 	break;
-	 	
+		for (int g = 0; g < ObjTypeList.size(); g++) 
+		{
+			ObjTypeList.get(g).printObjectiveType();
 		}
+			
+		System.out.println("What index would you like to update: ");
+		int updateIndex = input.nextInt();
+		
+		ObjectiveType obj = ObjTypeList.get(updateIndex-1);
+		ObjTypeList.get(updateIndex-1).printObjectiveType();
+		
+		System.out.println("What element would you like to change: ");
+		int element = input.nextInt();
+		
+		switch(element)
+		{
+			case 1:
+				System.out.println("Enter new/updated name: ");
+				obj.name= input.next();
+				break;
+			case 2:
+				System.out.println("Enter new/updated creation date: ");
+				obj.creation_date= input.nextInt();
+				break;
+			case 3:
+				System.out.println("Enter new/updated update date: ");
+				obj.updated_date= input.nextInt();
+				break;
+			case 4:
+				System.out.println("Enter new/updated active: ");
+				obj.active= input.next();
+				break;
+			case 5:
+				System.out.println("Enter new/updated creator name: ");
+				obj.creator_name= input.next();
+				break;
+			case 6:
+				System.out.println("Enter new/updated attribute list: ");
+				obj.attribute_list= input.next();
+				break;
+		}
+		ObjTypeList.set(updateIndex-1, obj);
+		System.out.println("Index data after update: ");	
+		System.out.println("Name" + "\t" + "Creation_date" + "\t" + "Updated_date" + "\t" + "Active" + "\t"
+				+ "Creator_name" + "\t" + "Attribute_list");
+		ObjTypeList.get(updateIndex-1).printObjectiveType();
+		
 		operations();
+	}
+	public static void delete() {
+		
 	}
 
 }
