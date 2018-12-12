4d26c68b03bbda402b013e24ae2abca1bb9bb363
nitish <nitish@nitish>
2018-12-11 19:29:38 -0500

    first test case written by Singh

---
+++ tests/rSMS/ObjectTypeTest.java
@@ -0,0 +1,52 @@
+package rSMS;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.sms.ObjectiveType;
+
+public class ObjectTypeTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+		
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		
+		
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testObjectiveType() {
+		fail("Not yet implemented");
+		
+		ObjectiveType obj = new ObjectiveType("Ksingh", "", month, date, hrs, min, sec) updateDate, active, creatorName, attList);
+		
+		assertEquals("Testing First Name" ,obj.toString(), "Ksingh");
+		
+	}
+
+	@Test
+	public void testPrintObjectiveType() {
+		fail("Not yet implemented");
+	}
+
+}
